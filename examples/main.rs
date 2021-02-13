c7794cdb25048b0ed472a68bc7f7a601a41005a3
Zettroke <skorostnoy2000@yandex.ru>
2021-02-13 11:39:03 +0300

    WIP

--- examples/main.rs
+++ examples/main.rs
@@ -1,4 +1,16 @@
-use const_writer::SliceConstWriter;
+#![feature(const_generics)]
+#![feature(const_evaluatable_checked)]
+#![feature(type_name_of_val)]
+
+
+
+use const_writer::{SliceConstWriter, ConstWrite};
 
 fn main() {
+    let mut buff = [0u8; 128];
+    let mut a = buff.const_writer::<256>();
+
+    let a = a.write_u16(1).write_u16(2).write_u16(3).write_u16(4).write_u16(5);
+
+    println!("{}", std::any::type_name_of_val(&a));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 #![feature(const_generics)]
 #![feature(const_evaluatable_checked)]
+#![feature(type_name_of_val)]
 #![allow(incomplete_features)]
+
+use std::marker::PhantomData;
+
 pub trait IsTrue {}
 pub trait IsFalse {}
 
@@ -10,41 +14,100 @@ impl IsTrue for Assert<true> {}
 impl IsFalse for Assert<false> {}
 
 pub struct SliceConstWriter<'a, const N: usize> {
-    ptr: &'a mut [u8]
+    ptr: *mut u8,
+    _marker: PhantomData<&'a ()>
+}
+
+macro_rules! advance_writer {
+    ($self:ident, $n:expr) => {
+        ConstWrite {
+            ptr: $self.ptr.add($n),
+            _marker: PhantomData
+        }
+    }
+}
+
+macro_rules! implement_write {
+    ($writer:ident, $name:ident, $type:ty, $size:expr) => {
+        impl<'a, const N: usize> $writer<'a, {N}> where Assert::<{N >= $size}>: IsTrue {
+            pub fn $name(self, value: $type) -> $writer<'a, {N - $size}> {
+                unsafe {
+                    std::ptr::copy_nonoverlapping(value.to_le_bytes().as_ptr(),self.ptr, $size);
+                    $writer {
+                        ptr: self.ptr.add($size),
+                        _marker: PhantomData
+                    }
+                }
+            }
+        }
+    }
 }
 impl<'a, const N: usize> SliceConstWriter<'a, {N}> {
-    pub fn from_slice(vec: &'a mut [u8]) -> Self {
-        Self {
-            ptr: vec
+    pub fn from_slice(value: &'a mut [u8]) -> Self {
+        if value.len() >= N {
+            Self {
+                ptr: value.as_mut_ptr(),
+                _marker: PhantomData
+            }
+        } else {
+            panic!();
         }
     }
+}
 
-    pub fn convert<const NN: usize>(mut self) -> SliceConstWriter<'a, {NN}> {
+pub trait ConstWriter<const N: usize> {
+    fn write_u8(self, value: u8) -> Self;
+}
+
+// impl<'a, const N: usize> ConstWriter<{N}> for SliceConstWriter<'a, {N}> {
+//     fn write_u8(self, value: u8) -> SliceConstWriter<'a, {N - 1}> {
+//         unsafe {
+//             std::ptr::copy_nonoverlapping(value.to_le_bytes().as_ptr(),self.ptr, 1);
+//             SliceConstWriter {
+//                 ptr: self.ptr.add(1),
+//                 _marker: PhantomData
+//             }
+//         }
+//     }
+// }
+
+implement_write!(SliceConstWriter, write_u16, u16, 2);
+implement_write!(SliceConstWriter, write_u32, u32, 4);
+
+pub trait ConstWrite {
+    fn const_writer<const N: usize>(&mut self) -> SliceConstWriter<{N}>;
+}
+
+impl ConstWrite for [u8] {
+    fn const_writer<const N: usize>(&mut self) -> SliceConstWriter<{ N }> {
+        assert!(
+            self.len() >= N,
+            "slice too short: {} < {}",
+            self.len(),
+            N
+        );
         SliceConstWriter {
-            ptr: self.ptr
+            ptr: self.as_mut_ptr(),
+            _marker: PhantomData
         }
     }
 }
 
-impl<'a, const N: usize> SliceConstWriter<'a, {N}> where Assert::<{N >= 2}>: IsTrue {
-    pub fn write_u16(mut self, value: u16) -> SliceConstWriter<'a, {N-2}> {
-        self.ptr as *mut u8;
-        SliceConstWriter {
-            ptr: self.ptr
-        }
-    }
-}
+
+
 
 
 #[cfg(test)]
 mod tests {
-    use crate::SliceConstWriter;
+    use crate::{SliceConstWriter, ConstWrite};
 
     #[test]
     fn it_works() {
         let mut buff = [0u8; 128];
-        let mut a = SliceConstWriter::<10>::from_slice(&mut buff);
+        let mut a = buff.const_writer::<10>();
 
-        let mut a = a.write_u8().write_u8().write_u8().write_u8().write_u8();
+        let a = a.write_u32(34).write_u16(3).write_u16(4).write_u16(5);
+
+        std::any::type_name_of_val(&a);
     }
 }
